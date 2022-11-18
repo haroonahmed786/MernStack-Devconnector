58fd6f80e0549e9b9326fbdd4d18a647391124e9
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-11-18 17:07:08 +0500

    experience profile

--- Frontend/src/components/dashboard/Dashboard.js
+++ Frontend/src/components/dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import { connect} from 'react-redux';
 import Spinner from '../layout/Spinner';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
 import { getCurrentProfile } from '../../actions/profile';
 
 
@@ -24,6 +25,7 @@ const Dashboard = ({ getCurrentProfile, auth: { user },  profile: {profile, load
       {profile !== null ? (
          <Fragment>
             <DashboardActions />
+            <Experience experience={profile.experience} />
          </Fragment>
       ) : (
       <Fragment>
